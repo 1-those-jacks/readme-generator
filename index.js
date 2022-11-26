51c797337111eb389fa6d63c0b4d469f0b72f0a2
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2022-11-25 21:41:12 -0600

    Filled out questions for README generator.

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		type: "input",
+		name: "title",
+		message: 'What is the name of this project?',
+	},
+	{
+		type: "input",
+		name: "description",
+		message: 'How would you describe this project?',
+	},
+	{
+		type: "input",
+		name: "installation",
+		message: 'How do you install this project?',
+	},
+	{
+		type: "input",
+		name: "usage",
+		message: 'How do you use this project?',
+	},
+	{
+		type: "input",
+		name: "contribution",
+		message: 'How can you contribute to this project?',
+	},
+	{
+		type: "input",
+		name: "tests",
+		message: 'How did you test this project?',
+	},
+	{
+		type: "input",
+		name: "github",
+		message: 'What github account can you be found at?',
+	},
+	{
+		type: "input",
+		name: "email",
+		message: 'What email can you be reached at?',
+	},
+	{
+		type: "list",
+		name: "license",
+		message: 'What license would you like to apply to this project?',
+		choices: ['none', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT'],
+	},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
